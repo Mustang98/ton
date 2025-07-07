a9b9a0b4929922e275c3dece20b9a39bd9f91326
olegvallas <oleg.vallas@gmail.com>
2025-07-07 10:42:21 +0000

    [improve-block-compression] Use new improved compression. Replace compression tl objects with V2 version in collator-node.

--- validator/collator-node.cpp
+++ validator/collator-node.cpp
@@ -631,9 +631,8 @@ tl_object_ptr<ton_api::collatorNode_Candidate> CollatorNode::serialize_candidate
         PublicKey{pubkeys::Ed25519{block.pubkey.as_bits256()}}.tl(), create_tl_block_id(block.id), block.data.clone(),
         block.collated_data.clone());
   }
-  size_t decompressed_size;
   td::BufferSlice compressed =
-      validatorsession::compress_candidate_data(block.data, block.collated_data, decompressed_size).move_as_ok();
+      validatorsession::compress_candidate_data(block.data, block.collated_data).move_as_ok();
   return create_tl_object<ton_api::collatorNode_compressedCandidateV2>(
       0, PublicKey{pubkeys::Ed25519{block.pubkey.as_bits256()}}.tl(), create_tl_block_id(block.id), std::move(compressed));
 }
